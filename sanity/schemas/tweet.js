1c47137288a1d1f7134f1afa86215dd6a39ea3e5
Charlton <charltonlucian@gmail.com>
2022-08-21 02:53:34 +0800

    initial commit

---
+++ sanity/schemas/tweet.js
@@ -0,0 +1,38 @@
+export default {
+  name: 'tweet',
+  title: 'Tweet',
+  type: 'document',
+  initialValue: {
+    blockTweet: false
+  },
+  fields: [
+    {
+      name: 'title',
+      title: 'Text in Tweet',
+      type: 'string',
+    },
+    {
+      name: 'blockTweet',
+      title: 'Block Tweet',
+      description: "ADMIN Controls: Toggle if tweet is deemed inappropriate",
+      type: 'boolean', 
+      
+      
+    },
+    {
+      name: 'username',
+      title: 'Username',
+      type: 'string',
+    },
+    {
+      name: 'profileImg',
+      title: 'Profile Image',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Tweet Image',
+      type: 'string',
+    }
+  ],
+}
