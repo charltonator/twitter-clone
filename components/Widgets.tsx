cab6a8d3e1b909c08a38484d88b00e0382583a34
Charlton <charltonlucian@gmail.com>
2022-08-22 00:40:46 +0800

    change widget

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -4,7 +4,7 @@ import {TwitterTimelineEmbed} from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div className="hidden lg:inline col-span-2 px-2 mt-2">
+    <div className="hidden lg:inline-block col-span-2 px-2 mt-2">
       <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2 mb-2">
         <SearchIcon className="h-5 w-5 text-gray-400" />
         <input
