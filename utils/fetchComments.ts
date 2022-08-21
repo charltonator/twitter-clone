65544deccf15c5c112b449c02d8f08efd4c99277
Charlton <charltonlucian@gmail.com>
2022-08-22 02:08:46 +0800

    bla

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -1,7 +1,7 @@
 import { Comment } from "../typings"
 
 export const fetchComments = async (tweetId: string) => {
-    const res = await fetch('https://twitter-clone-charlton.vercel.app/api/getComments?tweetId=${tweetId}')
+    const res = await fetch('https://twitter-clone-charlton.vercel.app/api/getComments?tweetId=$' + {tweetId})
 
     const comments: Comment[] = await res.json()
 
